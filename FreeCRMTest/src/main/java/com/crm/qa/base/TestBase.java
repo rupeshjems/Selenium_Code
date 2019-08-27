205bf03d07e2ed12d11a402f1cdd618471d40b72
Rupesh <rupeshjems@gmail.com>
2019-08-27 22:01:15 +0530

    First Commit

---
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,76 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import com.crm.qa.util.TestUtil;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+
+	
+	public TestBase()
+	{
+	
+		try {
+			prop=new Properties();
+			FileInputStream input=new FileInputStream("R:\\EclipseWorkspace\\eclipse-workspace\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			prop.load(input);
+			System.out.println("Hi Here");
+			
+		}
+		catch(FileNotFoundException fnfe)
+		{
+			fnfe.printStackTrace();	
+			System.out.println("Also Here");
+			
+		}
+		catch(IOException ioe)
+
+		{
+			ioe.printStackTrace();
+			System.out.println("Even Here");
+		}
+		
+	}
+	
+	public static void initilization()
+	{
+		String browserName=prop.getProperty("browser");
+		 
+		if(browserName.contentEquals("chrome"))
+		{
+			 
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rupesh\\Downloads\\chromedriver.exe");
+		 
+			
+		}
+		else if(browserName.contentEquals("Microsoft Edge"))
+		{
+			System.setProperty("webdriver.edge.driver", "C:\\Users\\Rupesh\\Downloads\\msedgedriver");
+			driver=new EdgeDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+		
+		
+	}
+	
+	
+	
+
+}
---
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,71 @@
+package com.crm.qa.pages;
+
+
+
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+	
+	// Page factory (Object Repository)
+	
+	//span[@class='icon icon-xs mdi-chart-bar']
+	
+	
+	
+	@FindBy(linkText="Login")
+	WebElement loginButton;
+	
+	
+	@FindBy(name = "email")
+	WebElement email;
+	
+	@FindBy(name = "password")
+	WebElement password;
+	
+	@FindBy(xpath= "//div[@class='ui fluid large blue submit button']")
+
+	WebElement login;
+	
+	
+	@FindBy(xpath= "//span[@class='mdi-chart-bar icon icon-md']")
+
+	WebElement signUpButton;
+	
+	@FindBy(xpath= "//div[@class='rd-navbar-brand']")
+
+	WebElement brandLogo;
+	
+	//Page object initilizarion
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Action
+	
+	public String validateLoginPageTitle()
+	{
+		return driver.getTitle();
+	}
+	
+	public boolean validateCrmImage() {
+		return brandLogo.isDisplayed();
+		 
+		
+	}
+	public HomePage login(String mail, String pwd)
+	{
+		email.sendKeys(mail);
+		password.sendKeys(pwd);
+		loginButton.click();
+		
+		return new HomePage();
+	} 
+	
+
+}
