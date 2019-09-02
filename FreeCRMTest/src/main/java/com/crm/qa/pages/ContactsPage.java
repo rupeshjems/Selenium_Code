ce4f1a34b058d484e23ac8494b0b27f2c65cc65c
Rupesh <rupeshjems@gmail.com>
2019-09-02 23:26:21 +0530

    Session 3 Commit

--- FreeCRMTest/src/main/java/com/crm/qa/pages/ContactsPage.java
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -1,5 +1,34 @@
 package com.crm.qa.pages;
 
-public class ContactsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+ 
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.crm.qa.base.TestBase;
+
+public class ContactsPage extends TestBase{
+	
+	
+	@FindBy(xpath=" //a[contains(text(), 'Contacts')]")
+	
+	WebElement contactsLabel;
+	 
+	public ContactsPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	public boolean verifyContactsLabel()
+	{
+		return contactsLabel.isDisplayed();
+	}
+	
+	public void selectContactsByName(String name)
+	{
+		driver.findElement(By.xpath(""));
+	}
+	
+	
+	
 }
--- FreeCRMTest/src/main/java/com/crm/qa/pages/HomePage.java
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,7 +1,69 @@
 package com.crm.qa.pages;
+ 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.crm.qa.base.TestBase;
 
 public class HomePage extends TestBase{
+	
 
+	@FindBy(xpath="//td[contains(text(),'User: Rupesh  SIngh')]")
+	
+	WebElement userNameLabel;
+	
+	@FindBy(xpath="//a[contains(text(), 'Contacts')]")
+	
+	WebElement contactsLink;
+	
+	@FindBy(xpath="//a[contains(text(), 'Deals')]")
+	
+	WebElement dealsLink;
+	
+	@FindBy(xpath="//a[contains(text(), 'Tasks')]")
+	
+	WebElement tasksLink;
+	
+	public void HomePage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String verifyHomePageTitle()
+	{
+		return driver.getTitle();
+	}
+	
+	public boolean verifyCorrectUserName()
+	{
+		 return userNameLabel.isDisplayed();
+	}
+	
+	public ContactsPage clickContectsLink()
+	{
+		contactsLink.click();
+		return new ContactsPage();
+		
+		
+	}
+	
+	public DealsPage clickDealsLink()
+	{
+		dealsLink.click();
+		return new DealsPage();
+		
+		
+	}
+	
+	public TasksPage clickTasksLink()
+	{
+		tasksLink.click();
+		return new TasksPage();
+		
+		
+	}
+	
+	
+	
 }
